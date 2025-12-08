376da456f4cd4c92db184abad60739d1aba82a47
patedde <122355697+patedde@users.noreply.github.com>
2025-12-08 13:05:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
+  // Replace with your GitHub Pages URL
+  site: 'https://patedde.github.io/paolotedde.website', 
+  // Replace with your repository name (starting with a slash)
+  base: '/paolotedde.website', 
+  
   integrations: [tailwind()],
 });
